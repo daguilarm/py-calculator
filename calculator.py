4a1ec7b11f92d9a8d52e0e5266703f68893c19ed
daguilarm <damian.aguilarm@gmail.com>
2021-05-21 13:05:46 +0200

    refactoring

--- calculator.py
+++ calculator.py
@@ -5,15 +5,16 @@ from screen import render as render_screen
 # Window
 root = Tk()
 root.title("Mac calculator app with Python")
+root.config(background='red')
 
 # Frame
 calculator = Frame(root)
-calculator.configure(background='black')
+calculator.configure(background='#333333')
 calculator.pack()
 
 # Screen
 # Default data to be show in the screen
-data = StringVar()
+data = StringVar(root, '0')
 # Render the screen with the default data
 screen = render_screen(data, calculator)
 
